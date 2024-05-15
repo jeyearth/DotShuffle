248cb56b91a0b530c72a8584b3c2f833852f8c95
JEY HIRANO <mrjr0722@me.com>
2024-05-15 22:28:36 +0900

    I made shuffleButton

--- PowerWordTwo/HomeView.swift
+++ PowerWordTwo/HomeView.swift
@@ -26,40 +26,46 @@ struct HomeView: View {
                     .frame(width: 340.0)
                 Spacer()
                 
-                //シャッフルボタン
-                VStack {
-                    Button {
-                        if data.words.count > 1 {
-                            while selectedNum == beforeNum {
-                                selectedNum = Int.random(in: 0...(data.words.count-1))
-                            }
-                            beforeNum = selectedNum
-                            selectedWord = data.words[selectedNum]
-                        } else {
-                            isShowAlert.toggle()
-                        }
-                    } label: {
-                        Image(systemName: "shuffle")
-                            .padding(.leading, 10)
-                            .padding(.trailing, 10)
-                    }
-                    .alert("Error", isPresented: $isShowAlert) {
-                    } message: {
-                        // アラートのメッセージ...
-                        if data.words.count == 1 {
-                            Text("ワードがひとつです。")
-                        } else if data.words.count == 0 {
-                            Text("ワードがありません。")
-                        }
-                    }
-                    .padding()
-                    .foregroundColor(Color.white)
-                    .background(Color.blue)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .shadow(radius: 7)
-                    .font(.title)
-                }
+                ShuffleButton(
+                    selectedWord: $selectedWord,
+                    selectedNum: $selectedNum,
+                    beforeNum: $beforeNum
+                )
                 .padding(.bottom, 100)
+//                //シャッフルボタン
+//                VStack {
+//                    Button {
+//                        if data.words.count > 1 {
+//                            while selectedNum == beforeNum {
+//                                selectedNum = Int.random(in: 0...(data.words.count-1))
+//                            }
+//                            beforeNum = selectedNum
+//                            selectedWord = data.words[selectedNum]
+//                        } else {
+//                            isShowAlert.toggle()
+//                        }
+//                    } label: {
+//                        Image(systemName: "shuffle")
+//                            .padding(.leading, 10)
+//                            .padding(.trailing, 10)
+//                    }
+//                    .alert("Error", isPresented: $isShowAlert) {
+//                    } message: {
+//                        // アラートのメッセージ...
+//                        if data.words.count == 1 {
+//                            Text("ワードがひとつです。")
+//                        } else if data.words.count == 0 {
+//                            Text("ワードがありません。")
+//                        }
+//                    }
+//                    .padding()
+//                    .foregroundColor(Color.white)
+//                    .background(Color.blue)
+//                    .clipShape(RoundedRectangle(cornerRadius: 20))
+//                    .shadow(radius: 7)
+//                    .font(.title)
+//                }
+//                .padding(.bottom, 100)
             } // VStackここまで
             HStack {
                 EditButton(selectedWord: $selectedWord)
--- PowerWordTwo/ShuffleButton.swift
+++ PowerWordTwo/ShuffleButton.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ShuffleButton: View {
+    @EnvironmentObject var data: WordData
+    @Binding var selectedWord: Word
+    @Binding var selectedNum: Int
+    @Binding var beforeNum: Int
+    
+    @State private var isShowAlert: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                print("shuffleButton!")
+                
+                if data.words.count > 1 {
+                    while selectedNum == beforeNum {
+                        selectedNum = Int.random(in: 0...(data.words.count-1))
+                    }
+                    beforeNum = selectedNum
+                    selectedWord = data.words[selectedNum]
+                } else {
+                    isShowAlert.toggle()
+                }
+                
+            } label: {
+                Image(systemName: "shuffle")
+                    .padding(.leading, 10)
+                    .padding(.trailing, 10)
+            }
+            .alert("Error", isPresented: $isShowAlert) {
+            } message: {
+                // アラートのメッセージ...
+                if data.words.count == 1 {
+                    Text("ワードがひとつです。")
+                } else if data.words.count == 0 {
+                    Text("ワードがありません。")
+                }
+            }
+            .padding()
+            .foregroundColor(Color.white)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .shadow(radius: 7)
+            .font(.title)
+        }
     }
 }
 
 #Preview {
-    ShuffleButton()
+    ShuffleButton(
+        selectedWord: .constant(Word()),
+        selectedNum: .constant(0),
+        beforeNum: .constant(0)
+    )
+        .environmentObject(WordData())
 }
