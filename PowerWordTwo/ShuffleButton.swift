1cba39a21c817f003067ace8439263e478cc2a83
JEY HIRANO <mrjr0722@me.com>
2024-05-15 22:24:19 +0900

    made shuffleButton UI

---
+++ PowerWordTwo/ShuffleButton.swift
@@ -0,0 +1,18 @@
+//
+//  ShuffleButton.swift
+//  DotShuffle
+//
+//  Created by 平野慈英 on 2024/05/15.
+//
+
+import SwiftUI
+
+struct ShuffleButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShuffleButton()
+}
