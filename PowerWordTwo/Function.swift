b5970de2b4f843333bfc9ec37341b04dbc465ab8
JEY HIRANO <mrjr0722@me.com>
2024-05-17 22:42:40 +0900

    I changed data construction

---
+++ PowerWordTwo/Function.swift
@@ -0,0 +1,8 @@
+//
+//  Function.swift
+//  DotShuffle
+//
+//  Created by 平野慈英 on 2024/05/17.
+//
+
+import Foundation
---
+++ PowerWordTwo/ListBlockView.swift
@@ -0,0 +1,18 @@
+//
+//  ListBlockView.swift
+//  DotShuffle
+//
+//  Created by 平野慈英 on 2024/05/17.
+//
+
+import SwiftUI
+
+struct ListBlockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListBlockView()
+}
