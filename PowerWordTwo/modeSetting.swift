c263c3f1832643777657654432aef3c11d387332
JEY HIRANO <mrjr0722@me.com>
2024-05-20 11:30:13 +0900

    I made ModeSetting.swift

--- PowerWordTwo/modeSetting.swift
+++ PowerWordTwo/modeSetting.swift
@@ -1,18 +1,18 @@
 //
-//  modeSetting.swift
+//  ModeSetting.swift
 //  DotShuffle
 //
-//  Created by 平野慈英 on 2024/05/18.
+//  Created by 平野慈英 on 2024/05/20.
 //
 
 import SwiftUI
 
-struct modeSetting: View {
+struct ModeSetting: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    modeSetting()
+    ModeSetting()
 }
