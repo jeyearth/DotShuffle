39ff9006372b6b8f7c564aac908f44e5178644b2
JEY HIRANO <mrjr0722@me.com>
2024-05-28 22:00:42 +0900

    オリジナルリスト作成を可能に。EditViewでリスト間の移動を可能に。

---
+++ PowerWordTwo/ListContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ListContentView.swift
+//  DotShuffle
+//
+//  Created by 平野慈英 on 2024/05/24.
+//
+
+import SwiftUI
+
+struct ListContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListContentView()
+}
