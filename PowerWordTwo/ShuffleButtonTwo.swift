19ab00a4f2c7008482423d393b13efcc2b5b0c44
JEY HIRANO <mrjr0722@me.com>
2024-06-30 20:56:08 +0900

    クラスShuffleDataを作成し、shuffleDataを利用してシャッフル表示をするSwiftファイルを作成しました。

---
+++ PowerWordTwo/ShuffleButtonTwo.swift
@@ -0,0 +1,18 @@
+//
+//  ShuffleButtonTwo.swift
+//  DotShuffle
+//
+//  Created by 平野慈英 on 2024/06/29.
+//
+
+import SwiftUI
+
+struct ShuffleButtonTwo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShuffleButtonTwo()
+}
