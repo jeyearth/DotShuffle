b79c69f4cb695dc35cc1c6cbe053507ed46bac27
JEY HIRANO <mrjr0722@me.com>
2024-07-03 21:38:55 +0900

    修正ボタンでのwordが変更できないバグを修正しました。

---
+++ PowerWordTwo/WordEditView.swift
@@ -0,0 +1,18 @@
+//
+//  WordEditView.swift
+//  DotShuffle
+//
+//  Created by 平野慈英 on 2024/07/01.
+//
+
+import SwiftUI
+
+struct WordEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WordEditView()
+}
