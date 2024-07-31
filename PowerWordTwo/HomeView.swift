3b69a9edd03876963ea10c6e34506bfececb8fee
JEY HIRANO <mrjr0722@me.com>
2024-07-31 16:00:29 +0900

    不要な変数をコメントアウトしました。

--- PowerWordTwo/HomeView.swift
+++ PowerWordTwo/HomeView.swift
@@ -11,9 +11,9 @@ struct HomeView: View {
     @EnvironmentObject var data: WordData
     @State var selectedWord: Word = Word()
     
-    @State private var selectedNum: Int = 0
-    @State private var beforeNum: Int = 0
-    @State private var beforeListNum: Int = 0
+//    @State private var selectedNum: Int = 0
+//    @State private var beforeNum: Int = 0
+//    @State private var beforeListNum: Int = 0
     
     @State private var showingSheet: Bool = false
     @State private var isShowAlert: Bool = false
